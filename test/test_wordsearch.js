0b69678baf01bdcdd795367bad45272e28b0fa6f
kenny-tse <kennytse91@gmail.com>
2021-09-23 00:53:55 +0000

    added functionality to wordsearch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,85 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present (vertically)", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present (backwards)", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'E', 'L', 'P', 'P', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present (horizontal)", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'A', 'P', 'P', 'L', 'E', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'E', 'L', 'X', 'X', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present (vertical top down)", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['P', 'A', 'X', 'X', 'L', 'E', 'R', 'G'],
+      ['P', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['L', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'E', 'L', 'X', 'X', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present (vertical bottom up)", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'E'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['X', 'A', 'X', 'X', 'L', 'E', 'R', 'P'],
+      ['X', 'H', 'C', 'S', 'Y', 'E', 'R', 'P'],
+      ['L', 'F', 'R', 'E', 'N', 'E', 'Y', 'A'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'E', 'L', 'X', 'X', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,58 @@
-const wordSearch = (letters, word) => { 
+//pair programmed with Claudia Wong https://github.com/fltfx/ 
+
+const wordSearch = (letters, word) => {
+    //Stretch word: Searching backwards
+    let wordBackwards = word.split('');
+    //console.log("wordBackwards", wordBackwards);
+    let arrayWordBackwards = wordBackwards.reverse();
+    //console.log("arrayWordBackwards", arrayWordBackwards);
+    wordBackwards = arrayWordBackwards.join('');
+    //console.log(wordBackwards);
+
+    //making one array of the 2D array and searches for the word horizontally
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word) || l.includes(wordBackwards)) return true
     }
+
+    //transposing the 2D array (to account for vertical occurences of word), and making one array of the 2D array and searches for the word horizontally again
+    let container = [];
+    const rows = letters[0].length;
+    const columns = letters.length;
+
+    //intialize y axis for the new transposed matrix
+    container.length = rows;
+    //using the container array, initialize the columns of the container array, so that it's now a 2D array
+    for (let i = 0; i < rows; i++) {
+        container[i] = Array(columns);
+    }
+
+    //transpose the input 'letters' matrix and store that in 'containers' matrix
+    for (let x = 0; x < letters.length; x++) {
+        for (let y = 0; y < letters[x].length; y++) {
+
+            container[y][x] = letters[x][y];
+        }
+    }
+
+    const horizontalJoinTransposed = container.map(ls => ls.join(''))
+    for (l of horizontalJoinTransposed) {
+        if (l.includes(word) || l.includes(wordBackwards)) return true
+    }
+
+    return false;
 }
+
+wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'E', 'L', 'P', 'P', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'APPLE');
 
 module.exports = wordSearch
